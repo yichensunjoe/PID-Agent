d5d85bd82a0d565c7d2b1f88001d86106f87b3e6
Joe.S <163158633+yichensunjoe@users.noreply.github.com>
2026-07-24 19:45:26 +0800

    feat: enable refined process connector interactions

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { ProcessInteractionEnhancements } from "./ProcessInteractionEnhancements";
 import { RuntimeEnhancements } from "./RuntimeEnhancements";
 import { UiCompatibilityEnhancements } from "./UiCompatibilityEnhancements";
 import "./styles.css";
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
     <RuntimeEnhancements />
+    <ProcessInteractionEnhancements />
     <UiCompatibilityEnhancements />
   </StrictMode>,
 );
