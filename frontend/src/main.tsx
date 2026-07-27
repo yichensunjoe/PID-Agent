b0438ee6454a30c980b633ff74f4caa24291ee03
Joe.S <163158633+yichensunjoe@users.noreply.github.com>
2026-07-27 14:00:31 +0800

    feat: mount vision image input enhancements

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { VisionImageInputEnhancements } from "./agent/VisionImageInputEnhancements";
 import { GridVisualCompatibility } from "./GridVisualCompatibility";
 import { ProcessInteractionEnhancements } from "./ProcessInteractionEnhancements";
 import { RuntimeEnhancements } from "./RuntimeEnhancements";
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
     <RuntimeEnhancements />
     <ProcessInteractionEnhancements />
     <GridVisualCompatibility />
+    <VisionImageInputEnhancements />
     <UiCompatibilityEnhancements />
   </StrictMode>,
 );
